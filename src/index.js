77c613e3904769cc9f3bea5e29929cfdc15d5713
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-02 15:28:48 -0400

    Document Object Model Working Explained

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import './index.css';
 import App from './App';
 
 const element = <h1>Hello World</h1>
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<React.StrictMode>
+  <App/>
+</React.StrictMode>, document.getElementById('root'));
