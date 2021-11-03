bec6391b0d088e6ac716347a5a24aa55f42fc9a4
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-03 13:29:12 -0400

    React State is used to save all data related to that Component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state={
+        count:0,
+    };
     render() { 
         return <>
-        <h1>Hello World</h1> 
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
         </>
     }
+
+    formatCount(){
+        const {count:ct} =this.state;
+        return ct === 0? <h5>Zero</h5>: <h5>{ct}</h5>;
+    }
 }
+
+
  
 export default Counter ;
