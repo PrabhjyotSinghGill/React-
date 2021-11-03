573ff9587c7b85d572fa68fcd4f32cb48910cdeb
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-03 11:01:30 -0400

    Spread Operator Explained on Objects

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-const first = [1,2,3];
-const second = [4,5,6];
+const first = {name:'Mosh'};
+const second = {job:'Instructor'};
 
-//const combined = first.concat(second);
-const combined = [...first,'a',...second,'b'];
+const combined = {...first,...second, location:'Australia'};
 
-const clone = [...first];
-console.log(first);
 console.log(combined);
