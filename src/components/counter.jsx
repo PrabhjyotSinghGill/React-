b0c4edece3030a5f3eeaf008f4fac5e2548d1e9d
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-03 15:10:57 -0400

    Changing Classes Dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,17 +5,24 @@ class Counter extends Component {
         count:0,
         logo:"favicon.ico"
     };
+
     render() { 
         return <>
-        <img className="m-1" src={this.state.logo} alt="" height="30px"></img>
-        <span class="badge badge-primary m-2">{this.formatCount()}</span>
+        <img className="m-1" src={this.state.logo} alt="" height="30em"></img>
+        <span style={{fontSize:10}} class={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
         </>
     }
 
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
     formatCount(){
         const {count:ct} =this.state;
-        return ct === 0? <h5>Zero</h5>: <h5>{ct}</h5>;
+        return ct === 0? "Zero": ct;
     }
 }
 
