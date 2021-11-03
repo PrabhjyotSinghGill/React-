62cc4ec988963efcdd0e67f330f777262cbfd6c4
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-03 16:14:19 -0400

    Rendering Lists Dynamilcally

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state={
         count:0,
-        logo:"favicon.ico"
+        logo:"favicon.ico",
+        tags:['tag1','tag2','tag3']
     };
 
     render() { 
@@ -11,6 +12,9 @@ class Counter extends Component {
         <img className="m-1" src={this.state.logo} alt="" height="30em"></img>
         <span style={{fontSize:10}} class={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+            {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+        </ul>
         </>
     }
 
