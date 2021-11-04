8ffe5797cd7761b293eb637e1b4b868546ef5bd7
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-04 16:15:49 -0400

    Binding Event Handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,23 +4,37 @@ class Counter extends Component {
     state={
         count:0,
         logo:"favicon.ico",
-        tags:['tag1','tag2','tag3']
+        tags:['0','1','2','3']
     };
+
+    constructor(){
+        super();
+        this.handleIncrement = this.handleIncrement.bind(this);
+    }
+
+    handleIncrement(){
+        console.log('Increment Clicked');
+    }
+
+    renderTags(){
+        if(this.state.tags.length === 0) return <p>'There are no Tags!'</p>;
+
+        return <ul>{this.state.tags.map(tag => <li key={tag}>
+            <img className="m-1" src={this.state.logo} alt="" height="25em"></img>
+            <span style={{fontSize:10}} className={this.getBadgeClasses(tag)}>{tag}</span>
+            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            </li>)}</ul>;
+    }
 
     render() { 
         return <>
-        <img className="m-1" src={this.state.logo} alt="" height="30em"></img>
-        <span style={{fontSize:10}} class={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-            {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-        </ul>
+        {this.renderTags()}
         </>
     }
 
-    getBadgeClasses() {
+    getBadgeClasses(tag) {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (tag === 0) ? "warning" : "primary";
         return classes;
     }
 
