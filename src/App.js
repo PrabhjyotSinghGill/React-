9f617f3816bfb720e4e9584f85108eff6d3a2c34
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-05 11:10:28 -0400

    Created a Movies Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Counter from './components/counter';
+import Movies from "./components/movies";
 
 function App() {
   return (
     <div className="app">
       {/* Header */}
-      <Counter></Counter>
+      {/*<Counter></Counter>*/}
+      <Movies></Movies>
     </div>
   );
 }
---
+++ src/components/movies.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Movies extends React.Component {
+    state = {};
+    render() { 
+        return <>Movies Component</>;
+    }
+}
+ 
+export default Movies;
