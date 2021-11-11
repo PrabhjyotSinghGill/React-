f01b051c3a05046b73d9452db1e2b4a584854c9b
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-11 15:13:40 -0500

    Added Bootstrap Grid + disabled functionality in decrement button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state={
-        value:this.props.counter.value,
-        tags:[0]
-    };
 
     /*constructor(){
         super();
@@ -20,28 +16,28 @@ class Counter extends Component {
     */
 
     renderTags(){
-        if(this.state.tags.length === 0) return <p>'Add Tags!'</p>;
-
-        return <ul className="list-group list-group-flush">{this.state.tags.map(tag => <li className="list-group-item" key={tag}>
-            {/*<img className="m-1" src={this.state.logo} alt="" height="25em"></img>*/}
-            {/*<h4>Counter {this.props.counter.id}</h4>*/}
-            <span style={{fontSize:10}} className={this.getBadgeClasses(this.props.counter)}>{this.formatCount()}</span>
-            <button 
-            onClick={
-                () =>{
-                this.props.onIncrement(this.props.counter);
-                }
-            }    
-            className="btn btn-dark btn-sm m-2">+</button>
-            <button 
-            onClick={
-                () =>{
-                this.props.onDecrement(this.props.counter);
-                }
-            }    
-            className="btn btn-dark btn-sm m-2">-</button>
-            <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">x</button>
-            </li>)}</ul>;
+        return (<div className="row">
+                    <div className="col-1">
+                    <span style={{fontSize:10}} className={this.getBadgeClasses(this.props.counter)}>{this.formatCount()}</span>
+                    </div>
+                    <div className="col">
+                    <button 
+                    onClick={
+                        () =>{
+                        this.props.onIncrement(this.props.counter);
+                        }
+                    }    
+                    className="btn btn-dark btn-sm m-2">+</button>
+                    <button 
+                    onClick={
+                        () =>{
+                        this.props.onDecrement(this.props.counter);
+                        }
+                    }    
+                    className="btn btn-dark btn-sm m-2" disabled={this.props.counter.value === 0 ? 'disabled' : ""}>-</button>
+                    <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">x</button>
+                </div>
+            </div>);
     }
 
     render() { 
@@ -58,7 +54,7 @@ class Counter extends Component {
 
     formatCount(){
         const {value:ct} =this.props.counter;
-        return ct === 0? "Zero": ct;
+        return ct === 0? "0": ct;
     }
 }
 
