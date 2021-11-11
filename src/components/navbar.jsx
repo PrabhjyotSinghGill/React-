485ab15dd39d0b3eb971422560fcda5b8c2e8882
Prabhjyot Singh Gill <Prabhjyotg@gmail.com>
2021-11-11 11:38:49 -0500

    Stateless Functional Components use props as a Argument rather than using this.props.counters call

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,14 @@
 import React, { Component } from 'react';
+/*Stateless Functional Component
+const Navbar = (props) =>{
+ return (
+            <nav className="navbar navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">
+                    Navbar <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
+                </a>
+            </nav>
+        );
+};*/
 
 class Navbar extends Component{
     render(){
